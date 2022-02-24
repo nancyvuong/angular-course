f4e13e836e9c4c295d9c73ea1e2e93f0c5322158
Nancy Vuong <vuongnancy8@gmail.com>
2022-02-24 15:03:02 -0800

    finished section 3

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'me'
+  password = ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
+
+
+  constructor(private router: Router, 
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+  handleLogin() {
+    //console.log(this.username);
+    //if(this.username=== "me" && this.password === "dummy"){
+    if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
+      //Redirect to Welcome Page
+      this.router.navigate(['welcome', this.username])
+      this.invalidLogin = false;
+    }
+
+    else{
+      this.invalidLogin = true;
+    }
+  }
+
+}
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+//import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  message = 'Some Welcome Message'
+  name = ''
+
+  //ActivatedRoute
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    //console.log(this.message)
+    //console.log(this.route.snapshot.params['name'])
+    this.name = this.route.snapshot.params['name']
+  }
+
+}
